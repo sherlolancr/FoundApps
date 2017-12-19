df6d1d21972b27021679148a2bf584265ff74738
qz3514 <qian.zheng14@imperial.ac.uk>
2017-12-19 11:34:17 +0000

    built a basic structure

--- src/Rover.java
+++ src/Rover.java
@@ -1,3 +1,62 @@
 public class Rover {
-    
+    private int x;
+    private int y;
+    private char directions;
+    int mapX;
+    int mapY;
+    public Rover(int x,int y,char directions,int mapX,int mapY){
+        this.x = x;
+        this.y = y; //constructor with initial position
+        this.directions = directions;
+        this.mapX = mapX;
+        this.mapY = mapY;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public char getDirections() {
+        return directions;
+    }
+
+    public void followInstruction(String s){
+        char[] instructions = s.toCharArray();
+        for (char c : instructions){
+            switch (c){
+                case 'F':
+                    moveForward();
+                    break;
+                case 'B':
+                    moveBack();
+                    break;
+                case 'L':
+                    turnLeft();
+                    break;
+                case 'R':
+                    turnRight();
+                    break;
+                default:System.out.println("One Wrong Instruction deteced :"+c);
+                         return;
+            }
+        }
+    }
+
+    private void turnLeft(){
+
+    }
+    private void moveBack(){
+
+    }
+    private void moveForward(){
+
+    }
+    private void turnRight(){
+
+    }
+
 }
