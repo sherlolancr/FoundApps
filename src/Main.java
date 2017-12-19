05828668134c5e3495e2bb7dfec7120171b0ddb3
qz3514 <qian.zheng14@imperial.ac.uk>
2017-12-19 11:53:16 +0000

    finished part one

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String args[]){
+        Rover a =new Rover(0,0,0.5*Math.PI,100,100);
+        a.followInstruction("FFRFF");
+
+    }
+}
--- src/Rover.java
+++ src/Rover.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
+
 public class Rover {
     private int x;
     private int y;
-    private char directions;
+    private double direction;
     int mapX;
     int mapY;
-    public Rover(int x,int y,char directions,int mapX,int mapY){
+    ArrayList<Character> directions = new ArrayList<Character>();
+
+    public Rover(int x,int y,double direction,int mapX,int mapY){
         this.x = x;
-        this.y = y; //constructor with initial position
-        this.directions = directions;
+        this.y = y; //constructor with initial position direction with degree in radian .X axis to East is 0
+        this.direction = direction;
         this.mapX = mapX;
         this.mapY = mapY;
     }
@@ -20,13 +24,16 @@ public class Rover {
         return y;
     }
 
-    public char getDirections() {
-        return directions;
+    public double getDirections() {
+        return direction;
     }
 
     public void followInstruction(String s){
         char[] instructions = s.toCharArray();
         for (char c : instructions){
+            System.out.println("Rover current at ("+x+" , "+y+")");
+            System.out.println("Rover current taking instruction :" +c);
+
             switch (c){
                 case 'F':
                     moveForward();
@@ -43,20 +50,27 @@ public class Rover {
                 default:System.out.println("One Wrong Instruction deteced :"+c);
                          return;
             }
+            System.out.println("Rover after instruction at ("+x+" , "+y+")");
+
         }
     }
 
     private void turnLeft(){
+        direction = direction + 0.5*Math.PI;
 
     }
     private void moveBack(){
-
+        x = (int)(x - Math.cos(direction));
+        y = (int)(y - Math.sin(direction));
     }
     private void moveForward(){
-
+        x = (int)(x + Math.cos(direction));
+        y = (int)(y + Math.sin(direction));
     }
     private void turnRight(){
+        direction = direction - 0.5*Math.PI;
 
     }
+
 
 }
